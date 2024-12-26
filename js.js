f09f968cb98b18a3243323dbdf437ae218fa9366
ArshiaArshadi <arshia.arshadi1384@gmail.com>
2024-12-26 18:33:52 +0330

    first commit

---
+++ js.js
@@ -0,0 +1,26 @@
+    //           🧀           
+    var cheese = "Mozzarella";
+    cheese = "Cheddar"; // تغییر مقدار
+    console.log(cheese); // خروجی: Cheddar
+    
+    
+    
+    
+    //           🔥
+    let ovenTemperature = 200; // دمای اولیه
+    ovenTemperature = 220; // تغییر مقدار
+    console.log(ovenTemperature); // خروجی: 220
+    
+    
+    
+    
+    
+    
+    //           📜
+    const recipe = "Classic Pizza";    
+    recipe = "Vegan Pizza"; // خطا!
+    console.log(recipe); // خروجی: Classic Pizza
+    
+
+
+
